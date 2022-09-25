6691a2ab23cd406b3e4fc8c20d9d463ce3da0d3f
xiaopi826 <1076870479@qq.com>
2022-09-25 17:37:24 +0800

    Site updated: 2022-09-25 17:37:24

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
-        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var a = new Array("耐得住寂寞，才能守得住繁华", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -12,7 +12,7 @@ jQuery(document).ready(function ($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
         });
         $("body").append($i);
         $i.animate({
